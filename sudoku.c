0b73c533ccdee8f3216b96dd2e520b6a54a3cf0c
El Chappell <e.chappell1@lancaster.ac.uk>
2026-07-15 21:36:08 +0100

    cleaned up algorithm to encapsulate print method & work w/ differing sizes

--- sudoku.c
+++ sudoku.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 
 #define EXIT_FAILURE 1
+#define SIZE 9
 
-int grid[9][9];
+int grid[SIZE][SIZE];
 
 int loadPuzzle (){
     FILE* f;
@@ -11,8 +12,8 @@ int loadPuzzle (){
 
     f = fopen(filename, "r");
     if (f != NULL){
-        for (int i = 0; i < 9; i++){
-            for (int j = 0; j < 9; j++){
+        for (int i = 0; i < SIZE; i++){
+            for (int j = 0; j < SIZE; j++){
                 if (fscanf(f, "%d", &grid[i][j]) != 1){
                     fclose(f);
                     return -1;
@@ -27,6 +28,21 @@ int loadPuzzle (){
     }
 }
 
+void printPuzzle(){
+    for (int i = 0; i < SIZE; i++){
+        for (int j = 0; j < SIZE; j++){
+            printf("%d ", grid[i][j]);
+            if((j + 1) % 3 == 0 && j != SIZE - 1){
+                printf("| ");
+            }
+        }
+        if ((i + 1) % 3 == 0 && i != SIZE - 1){
+            printf("\n----------------------");
+        }
+        printf("\n");
+    }
+}
+
 int main(int argc, char* argv[]){
     int puzzleError = loadPuzzle();
     if (puzzleError == -1){
@@ -34,11 +50,6 @@ int main(int argc, char* argv[]){
         exit(1);
     }
     else{
-        for (int i = 0; i < 9; i++){
-            for (int j = 0; j < 9; j++){
-                printf("%d ", grid[i][j]);
-            }
-            printf("\n");
-        }
+        printPuzzle();
     }
 }
