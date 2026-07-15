67c6622b4b889782c143a312f6fe5e2a862e43a0
El Chappell <e.chappell1@lancaster.ac.uk>
2026-07-15 22:46:19 +0100

    added function to populate initial sparse matrix from pre-existing grid (need to tackle prefilled cells still)

--- sudoku.c
+++ sudoku.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 
 #define EXIT_FAILURE 1
-#define SIZE 9
+#define SIZE 3
+#define N (SIZE * SIZE)
 
-int grid[SIZE][SIZE];
+int grid[N][N];
 
 int loadPuzzle (){
     FILE* f;
@@ -12,8 +13,8 @@ int loadPuzzle (){
 
     f = fopen(filename, "r");
     if (f != NULL){
-        for (int i = 0; i < SIZE; i++){
-            for (int j = 0; j < SIZE; j++){
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
                 if (fscanf(f, "%d", &grid[i][j]) != 1){
                     fclose(f);
                     return -1;
@@ -29,17 +30,69 @@ int loadPuzzle (){
 }
 
 void printPuzzle(){
-    for (int i = 0; i < SIZE; i++){
-        for (int j = 0; j < SIZE; j++){
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
             printf("%d ", grid[i][j]);
-            if((j + 1) % 3 == 0 && j != SIZE - 1){
+            if((j + 1) % SIZE == 0 && j != N - 1){
                 printf("| ");
             }
         }
-        if ((i + 1) % 3 == 0 && i != SIZE - 1){
+        if ((i + 1) % SIZE == 0 && i != N - 1){
             printf("\n----------------------");
         }
         printf("\n");
+    }
+}
+
+void createMatrix(){
+    int prefilled[N][3];
+    int pfilledCtr = 0;
+
+    for (int R = 0; R < N; R++){
+        for (int C = 0; C < N   ; C++){
+            if (grid[R][C] > 0){
+                prefilled[pfilledCtr][0] = grid[R][C];
+                prefilled[pfilledCtr][1] = R;
+                prefilled[pfilledCtr][2] = C;
+                pfilledCtr++;
+                printf("Prefilled: %d at Row: %d, Col: %d\n", grid[R][C], R, C);
+            }
+        }
+    }
+
+    int matrix[N*N*N][4*N*N];
+    // N^3 given representing row, column and number possibilities as matrix rows
+    // N^2 given repersenting 4 contraints across rows and columns as matrix columns
+    for (int V = 0; V < N; V++){
+        // iterate through each number/value possibility
+        for (int R = 0; R < N; R++){
+            // iterate through each row possibility
+            for (int C = 0; C < N; C++){
+                // iterate through each column possibility
+                if (grid[R][C] == 0){
+                    // if cell is not prefilled
+                    int rowIndex = C + (N * R) + (N * N * V);
+                    int blockIndex = (C / SIZE) + ((R / SIZE) * SIZE);
+                    // column indexes for constraints per cell, row, column and box
+                    int indexCell = R * N + C;
+                    // calculate row with N aka 1st row = 0, 2nd = 1 and then add column to get unique index for each cell
+                    int indexRow = N * N + R * N + V;
+                    // size^2 to be past index of indexCell then r * size to represent grid index and V to represent value at index
+                    int indexCol = 2 * N * N + C * N + V;
+                    // same as above but for columns instead of rows and added offset of another size^2
+                    int indexBox = 3 * N * N + (blockIndex * N + V);
+                    // 3 * size^2 to be past index (know 3 indexes taking up N^2 space)
+
+                    // fill all constraints with a 1
+                    matrix[rowIndex][indexCell] = 1;
+                    matrix[rowIndex][indexRow] = 1;
+                    matrix[rowIndex][indexCol] = 1;
+                    matrix[rowIndex][indexBox] = 1;
+
+                    printf("Row: %d, Col: %d, Val: %d all filled w/ 1s as empty\n", R, C, V + 1);
+                }
+            }
+        }
     }
 }
 
@@ -51,5 +104,6 @@ int main(int argc, char* argv[]){
     }
     else{
         printPuzzle();
+        createMatrix();
     }
 }
